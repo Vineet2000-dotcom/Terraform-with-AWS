c60e25c85d9472777004e6f0903b911e7293fc5e
VINEET IYER <vineetiyer306@gmail.com>
2025-10-19 21:53:09 +0530

    new changes to app.sh

---
+++ app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# This is a simple shell script
+
+echo "Hello, Vineet!"
+echo "Listing all files in the current directory..."
+ls
+
+echo "Current date and time:"
+date
+
+echo "Script executed successfully!"
